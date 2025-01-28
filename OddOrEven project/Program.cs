f76c7f63fc95f1e1ba5ccf920100995ed5c45bc4
benmakescode <73973708+BenRedmondgithub@users.noreply.github.com>
2025-01-28 13:05:15 +0000

    Uploaded an odd or even program
    pog

---
+++ OddOrEven project/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Transactions;
+
+static void Main(string[] args)
+{
+    
+    Console.WriteLine("enter an integer: ");
+    string input = Console.ReadLine();
+
+    if (int.TryParse(input, out int number))
+    {
+      if (number % 2 == 0)
+      {
+        Console.WriteLine($"{number} is an Even Number"); 
+      }
+
+      else 
+      {
+        Console.WriteLine($"{number} is an Odd Number");
+      }
+    }  
+
+      else
+      {
+        Console.WriteLine("Invalid Input, Please enter another number");
+      } 
+
+    Console.ReadLine();
+}
+
