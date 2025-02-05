f052b8a6fd4779b61157d5b9e3b7e4afa1899f6a
benmakescode <73973708+BenRedmondgithub@users.noreply.github.com>
2025-02-05 22:24:52 +0000

    updated files

--- authorApp/Program.cs
+++ authorApp/Program.cs
@@ -37,17 +37,63 @@ namespace authorApp
     {
         foreach (var resArea in ReseachArea)
         {
-            Console.WriteLine(resArea);
+            Console.WriteLine("- " + resArea);
         }
+    }
+
+    public void showBooks()
+    {
+        foreach (var book in Books)
+        {
+            Console.WriteLine("- " +book);
+        }
+    }
+
+    public void showSeminarSchedule()
+    {
+        foreach (var seminar in seminarSchedule)
+        {
+            Console.WriteLine("- " +seminar);
+        }
+    }
+
+    public void showAuthorInfo()
+    {
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Qualification: " + Qualification);
+        Console.WriteLine("Working Place: " + WorkingPlace);
+        Console.WriteLine("Country: " + Country);
+        Console.WriteLine("Books: ");
+        showBooks();
+        Console.WriteLine("Research Area: ");
+        showResArea();
+        Console.WriteLine("Seminar Schedule: ");
+        showSeminarSchedule();
     }
 
     class Program 
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Size of int: {0}", sizeof(int));
 
-            Console.ReadLine();
+            List<string> Books = new List<string>();
+            Books.Add("Book1");
+            Books.Add("Book2");
+            Books.Add("Book3");
+
+            List<string> ReseachArea = new List<string>();
+            ReseachArea.Add("Area1");
+            ReseachArea.Add("Area2");
+            ReseachArea.Add("Area3");
+
+            List<string> seminarSchedule = new List<string>();
+            seminarSchedule.Add("Seminar1");
+            seminarSchedule.Add("Seminar2");
+            seminarSchedule.Add("Seminar3");
+
+            Author author = new Author("Author1", "PhD", "University1", "Country1", Books, ReseachArea, seminarSchedule);
+            author.showAuthorInfo();
+
         }
     }
 }
